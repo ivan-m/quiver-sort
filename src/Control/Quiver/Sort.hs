7b0d2fd270b8e76e3ef534f26cfae0adab8a6e51
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-03-30 15:35:07 +1100

    Initial package setup

---
+++ src/Control/Quiver/Sort.hs
@@ -0,0 +1,11 @@
+{- |
+   Module      : Control.Quiver.Sort
+   Description : Sort values in a Quiver
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : MIT
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+
+
+ -}
+module Control.Quiver.Sort where
