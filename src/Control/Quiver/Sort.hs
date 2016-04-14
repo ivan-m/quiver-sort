47b526f62450b23d16658d486de6f8c77a0690bf
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-04-14 17:10:16 +1000

    Constrain the number of temp files read at a time

--- src/Control/Quiver/Sort.hs
+++ src/Control/Quiver/Sort.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE PatternSynonyms, RankNTypes, ScopedTypeVariables, ViewPatterns #-}
+
 {- |
    Module      : Control.Quiver.Sort
    Description : Sort values in a Quiver
@@ -24,6 +25,7 @@ module Control.Quiver.Sort (
   , defaultConfig
   , setChunkSize
   , setTempDir
+  , setMaxFiles
   ) where
 
 import Control.Quiver.Binary
@@ -33,20 +35,28 @@ import Control.Quiver.Instances  ()
 import Control.Quiver.Interleave
 import Control.Quiver.SP
 
-import Control.Applicative          (liftA2)
-import Control.Exception            (IOException, finally)
-import Control.Monad                (join)
-import Control.Monad.Catch          (MonadCatch (..), MonadMask)
-import Control.Monad.IO.Class       (MonadIO (..))
-import Control.Monad.Trans.Resource (MonadResource, allocate)
-import Data.Bool                    (bool)
-import Data.Function                (on)
-import Data.List                    (sortBy)
-import System.Directory             (doesDirectoryExist, getPermissions,
-                                     getTemporaryDirectory,
-                                     removeDirectoryRecursive, writable)
-import System.IO                    (hClose, openTempFile)
-import System.IO.Temp               (createTempDirectory)
+import           Control.Applicative          (liftA2)
+import           Control.Exception            (IOException)
+import           Control.Monad                (join)
+import           Control.Monad.Catch          (MonadCatch (..), MonadMask,
+                                               finally)
+import           Control.Monad.IO.Class       (MonadIO (..))
+import           Control.Monad.Trans.Resource (MonadResource, allocate)
+import           Data.Bool                    (bool)
+import           Data.Coerce                  (coerce)
+import           Data.Foldable                (toList)
+import           Data.Function                (on)
+import           Data.List                    (sortBy)
+import           Data.Monoid                  (First (..), (<>))
+import           Data.Sequence                (Seq, (|>))
+import qualified Data.Sequence                as S
+import           System.Directory             (doesDirectoryExist,
+                                               getPermissions,
+                                               getTemporaryDirectory,
+                                               removeDirectoryRecursive,
+                                               writable)
+import           System.IO                    (hClose, openTempFile)
+import           System.IO.Temp               (createTempDirectory)
 
 --------------------------------------------------------------------------------
 
@@ -63,13 +73,16 @@ spsort = spsortBy compare
 
 -- | Use the specified comparison function to sort the values.
 spsortBy :: (Monad m) => (a -> a -> Ordering) -> SP a a m ()
-spsortBy f = (sortBy f <$> spfoldr (:) []) >>= spevery
+spsortBy f = (sortBy f <$> spToList) >>= spevery
 
 -- | Use the provided function to be able to compare values.
 spsortOn :: (Ord b, Monad m) => (a -> b) -> SP a a m ()
 spsortOn f = sppure ((,) <*> f)
              >->> spsortBy (compare `on` snd)
              >->> sppure fst >&> snd
+
+spToList :: SQ a x f [a]
+spToList = spfoldr (:) []
 
 --------------------------------------------------------------------------------
 
@@ -91,6 +104,9 @@ data SPFileConfig = FC { _chunkSize  :: !Int
                          -- cleaned up afterwards.  'Nothing'
                          -- indicates to use the system temporary
                          -- directory.
+                       , _maxFiles   :: !Int
+                         -- ^ The maximum number of temporary files to
+                         -- be open at any one time.
                        }
 
 -- | Default settings for sorting using external files:
@@ -98,9 +114,12 @@ data SPFileConfig = FC { _chunkSize  :: !Int
 --   * Have a chunk size of @1000@.
 --
 --   * Use the system temporary directory.
+--
+--   * No more than @100@ temporary files to be open at a time.
 defaultConfig :: SPFileConfig
 defaultConfig = FC { _chunkSize  = 1000
                    , _withTmpDir = Nothing
+                   , _maxFiles   = 100
                    }
 
 -- | Specify the size of chunks to be individually sorted: the larger
@@ -115,6 +134,14 @@ setChunkSize cs cfg = cfg { _chunkSize = cs }
 --   directory isn't large or fast enough.
 setTempDir :: FilePath -> SPFileConfig -> SPFileConfig
 setTempDir dir cfg = cfg { _withTmpDir = Just dir }
+
+-- | The maximum number of files that should be open at any one time.
+--
+--   Larger values will be faster, but run the risk of exhausting the
+--   operating system's supply of file descriptors (and thus being
+--   killed).
+setMaxFiles :: Int -> SPFileConfig -> SPFileConfig
+setMaxFiles c cfg = cfg { _maxFiles = c }
 
 -- | Use external files to temporarily store partially sorted results
 -- (splitting into chunks of the specified size if one is provided).
@@ -144,31 +171,50 @@ spfilesortBy cmp cfg = do mdir' <- join <$> liftIO (traverse checkDir (_withTmpD
 
     getTmpDir = maybe withSystemTempDirectory withTempDirectory
 
-    pipeline tmpDir = toFiles tmpDir >>= either spfailed (sortFromFiles cmp)
+    pipeline tmpDir = toFiles tmpDir >>= either spfailed (sortFromFiles maxFiles cmp tmpDir)
 
-    toFiles tmpDir = sortToFiles chunkSize cmp tmpDir >->> spToList >&> uncurry (flip checkFailed)
+    toFiles tmpDir = sortToFiles chunkSize cmp tmpDir >->> spToSeq >&> uncurry (flip checkFailed)
 
     chunkSize = _chunkSize cfg
 
-sortToFiles :: (Binary a, MonadIO m) => Int -> (a -> a -> Ordering) -> FilePath
+    maxFiles = _maxFiles cfg
+
+sortToFiles :: (Binary a, MonadIO m, MonadMask m) => Int -> (a -> a -> Ordering) -> FilePath
                -> SP a FilePath m IOException
 sortToFiles chunkSize cmp tmpDir = spchunks chunkSize
                                    >->> spTraverseUntil sortChunk
                                    >&> snd
   where
-    sortChunk as = liftIO $ do (fl,h) <- openTempFile tmpDir "quiver-sort-chunk"
-                               finally (checkFailed fl <$> sprun (pipeline h)) (hClose h)
-      where
-        pipeline h = spevery (sortBy cmp as) >->> spencode >->> qPut h >&> snd
+    sortChunk as = writeOut tmpDir (spevery (sortBy cmp as))
 
-sortFromFiles :: (Binary a, MonadIO m) => (a -> a -> Ordering) -> [FilePath]
-                 -> Producer a () m (SPResult IOException)
-sortFromFiles cmp fls = spinterleave cmp (map readFl fls)
+writeOut :: (Binary a, MonadIO m, MonadMask m) => FilePath -> P () x a () m (SPResult IOException)
+            -> m (Either IOException FilePath)
+writeOut tmpDir p = do (fl,h) <- liftIO (openTempFile tmpDir "quiver-sort-chunk")
+                       finally (checkFailed fl <$> sprun (pipeline h)) (liftIO (hClose h))
   where
+    pipeline h = p >->> spencode >&> fst >->> qhoist liftIO (qPut h) >&> getFirstError
+
+sortFromFiles :: (Binary a, MonadIO m, MonadMask m) => Int -> (a -> a -> Ordering) -> FilePath
+                 -> Seq FilePath -> Producer a () m (SPResult IOException)
+sortFromFiles mf cmp tmpDir = nextBatch
+  where
+    nextBatch Empty = spcomplete
+    nextBatch fls   = case S.splitAt mf fls of
+                        (b,Empty) -> batch b
+                        (b,fls')  -> do br <- qlift (writeBatch b)
+                                        either spfailed (nextBatch . (fls' |>)) br
+
+    writeBatch = writeOut tmpDir . batch
+
+    batch = spinterleave cmp . map readFl . toList
+
     -- Assume decoding is successful for now.
     readFl fl = qhoist liftIO (qReadFile fl readSize) >->> spdecode >&> fst
 
     readSize = 4096
+
+-- Just to make it nicer to pattern-match
+pattern Empty <- (S.viewl -> S.EmptyL)
 
 spTraverseUntil :: (Monad m) => (a -> m (Either e b)) -> SP a b m e
 spTraverseUntil k = loop
@@ -181,8 +227,8 @@ checkFailed :: r -> SPResult e -> Either e r
 checkFailed _ (Just (Just e)) = Left e
 checkFailed r _               = Right r
 
-spToList :: SQ a x f [a]
-spToList = spfoldr (:) []
+spToSeq :: SQ a x f (Seq a)
+spToSeq = spfoldl' (|>) mempty
 
 --------------------------------------------------------------------------------
 -- Creating the temporary directory
@@ -205,3 +251,9 @@ withTempDirectory targetDir template withTmp = do
 
 ignoringIOErrors :: (MonadCatch m) => m () -> m ()
 ignoringIOErrors ioe = ioe `catch` (\(_ :: IOError) -> return ())
+
+getFirstError :: (SPResult a, SPResult a) -> SPResult a
+getFirstError (r1,r2) = coerce (toFirst r1 <> toFirst r2)
+  where
+    toFirst :: SPResult a -> Maybe (First a)
+    toFirst = coerce
